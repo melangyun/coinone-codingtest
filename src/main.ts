87e858a9bba4b387432b2b76e3217602b44541be
melangyun <melangyun@gmail.com>
2024-01-29 18:09:08 +0900

    feat: swagger, transformer, validator추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn'],
+    logger: ['error', 'warn', 'log'],
   });
+
+  // validation 설정
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  // swagger 설정
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('NestJS API')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
